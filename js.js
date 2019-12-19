8be92d22d4b995159a1ea521058fce6fb84fec0b
Chai <isaigomezhernandez.com>
2019-12-18 19:46:20 -0600

    Funciona bien

--- js.js
+++ js.js
@@ -1,15 +1,22 @@
+/*
+Dada una matriz donde el primer elemento es el número de filas y columnas. Hacer la suma 
+en forma de X. Entrada [3,[1,2,3],[1,2,3],[1,2,3]]; salida (1+2+3) entre (3+2+1).
+Example: 
+A b C
+c B a
+B a C
+
+La primera suma se conforma por A+B+C.
+La segunda suma se conforma por C+B+B.
+Obteniendo los dos totales se suman y se divide entre 2 returnando el resultado.
+*/
 //entrada
 const input = [4, [5, 2, 5, 1], [1, 5, 3, 1], [5, 2, 5, 1], [5, 3, 5, 1]];
-console.log(input[1][0]);
-// ver los elementos de la entrada
-input.forEach((element, index) => {
-  console.log(element);
-  //console.log(index);
-});
+
 //Almacenar la suma de cada elemnto en diagonal
 let firstLine = 0;
 let secontLine = 0;
-// hacer la media de cada diagonal
+// Hacer la media entre los dos totales.
 function mediaMatrix(ars) {
   for (let i = 1; i <= ars[0]; i++) {
     firstLine += ars[i][i - 1];
@@ -17,6 +24,6 @@ function mediaMatrix(ars) {
   for (let i = ars[0]; i > 0; i--) {
     secontLine += ars[i][ars[i].length - i];
   }
-  return [firstLine, secontLine];
+  return (firstLine + secontLine) / 2;
 }
 console.log(` valor de la function ${mediaMatrix(input)}`);
